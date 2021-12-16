06304ed3bdbaa3678d050d0c239e91d85fe11170
edvasilev94 <94469360+edvasilev94@users.noreply.github.com>
2021-12-16 12:05:07 +0200

    add validation for create

--- src/components/CreateRecipe/CreateRecipe.js
+++ src/components/CreateRecipe/CreateRecipe.js
@@ -1,11 +1,14 @@
+import {useState} from "react";
 import { useAuthContext } from "../../contexts/AuthContext";
 import { useNavigate } from "react-router-dom";
 import * as recipeService from "../../services/recipesService"
+
 
 const CreateRecipe = () => {
 
 	const { user } = useAuthContext();
     const navigate = useNavigate();
+	const [error, setError] = useState({message: null});
 
     const onSubmitHandler = (e) => {
         e.preventDefault();
@@ -18,20 +21,34 @@ const CreateRecipe = () => {
         let steps = formData.get("steps");
 
 		
+		if(name === "" 
+			|| ingredients === ""
+			|| timetocook === ""
+			|| img ===""
+			|| steps === ""){
+			setError(state =>({
+				...state,
+				message: "All fileds are requiered!"
+			}))
 			
-				recipeService.create({
-					name,
-					ingredients: ingredients.split(", "),
-					timetocook,
-					img,
-					steps: steps.split(", ")
-				}, user.accessToken)
-					.then(x => {
-						navigate("/recipes");
-					})
-    }
-
-
+		} else {
+			recipeService.create({
+				name,
+				ingredients: ingredients.split(", "),
+				timetocook,
+				img,
+				steps: steps.split(", ")
+			}, user.accessToken)
+			.then(x => {
+				navigate("/recipes");
+			})
+			}
+		}	
+	
+		
+let errorContainer = (<div className="errorContainer">
+						<p>{error.message}</p>
+	 			     </div>)
 
     return (
         <div className="banner">
@@ -56,6 +73,11 @@ const CreateRecipe = () => {
 							<div className="form-group">
 								<input type="text" className="form-control" name="steps" id="steps" placeholder="Instructions" />
 							</div>
+							{ error.message
+                        			? errorContainer
+                       			    : null
+                  		    }
+
 							<button type="submit" className="btn btn-default">Create</button>&nbsp;
 						</form>
 					</div>
