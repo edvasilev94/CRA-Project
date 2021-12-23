0111c8debd0f09822718e7dac569a852a7bc5727
edvasilev94 <94469360+edvasilev94@users.noreply.github.com>
2021-12-23 09:06:52 +0200

    remove comments

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -20,13 +20,6 @@ const Footer = () => {
                         ? userFooter
                         : guestFooter
                     }
-            {/* <div className="social">
-                <a href="#"><i className="fa fa-facebook"></i></a>
-                <a href="#"><i className="fa fa-twitter"></i></a>
-                <a href="#"><i className="fa fa-dribbble"></i></a>
-                <a href="#"><i className="fa fa-linkedin"></i></a>
-                <a href="#"><i className="fa fa-google-plus"></i></a>
-            </div> */}
             <p className="copy-right">Copyright &copy; 2021 <a href="https://softuni.bg/">SoftUni</a> | Designed By : <a href="https://github.com/edvasilev94">Encho Vasilev</a>, All rights reserved. </p>
         </div>
     </footer>
